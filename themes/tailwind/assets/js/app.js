bb95043687fd118799fb9e669793c9dd1b31785e
taoguangc <taoguangc@gmail.com>
2022-04-12 12:29:45 +0800

    import alpinejs

--- themes/tailwind/assets/js/app.js
+++ themes/tailwind/assets/js/app.js
@@ -1,6 +1,6 @@
-// import Alpine from 'alpinejs'
-// window.Alpine = Alpine
-// Alpine.start()
+import Alpine from 'alpinejs'
+window.Alpine = Alpine
+Alpine.start()
 
 import sal from 'sal.js'
 sal()
